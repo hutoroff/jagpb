d7d6389fe3b3a89fe37852550588711e194e13ec
hutoroff <hutoroff@gmail.com>
2018-02-26 01:31:37 +0300

    typo

--- src/main/java/ru/hutoroff/jagpb/bot/commands/Help.java
+++ src/main/java/ru/hutoroff/jagpb/bot/commands/Help.java
@@ -5,7 +5,7 @@ public class Help {
 	public static final String COMMAND_HELP_COMMAND = "usage: /commandHelp COMMAND";
 	public static final String CREATE_POLL_COMMAND = "usage: /create -t \"Poll Title\" -o {\"option1\",\"option2\",...,\"optionN\"}";
 	public static final String HELP = "usage: /help";
-	public static final String LAST_POLL = "usage: /lastPollResult [-c \"CHAT_ID\"]";
+	public static final String LAST_POLL = "usage: /lastPollResult [[-c \"CHAT_ID\"]]";
 	public static final String START_COMMAND = "usage: /start";
 
 }
